549cf5d89d5c358f3bc571df8b628c34ce4e088f
Marjune <marjune@mrjbtc.local>
2025-09-01 11:22:03 +0100

    Updating src/LinkedList.cpp on how reverse linked list works

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -26,6 +26,56 @@ namespace NS_LinkedList {
 		}
 	}
 
+	/*
+		Linked list: 1 -> 2 -> 3 -> 4 -> 5 -> nullptr
+
+		** reversing the linked list **
+
+		prev -> current -> next
+
+		[1] current 1
+				point the current to previous
+				1 -> nullptr
+
+				switch previous to current and current to next
+			previous 1
+			current 2
+
+		[2] current 2
+				point the current to previous
+				2 -> 1 -> nullptr
+
+				switch previous to current and current to next
+			previous 2
+			current 3
+		
+		[3] current 3
+				point the current to previous
+				3 -> 2 -> 1 -> nullptr
+
+				switch previous to current and current to next
+			previous 3
+			current 4
+		
+		[4] current 4
+				point the current to previous
+				4 -> 3 -> 2 -> 1 -> nullptr
+
+				switch previous to current and current to next
+			previous 4
+			current 5
+
+		[5] current 5
+				point the current to previous
+
+				5 -> 4 -> 3 -> 2 -> 1 -> nullptr
+
+				switch previous to current and current to next
+			previous 5
+			current nullptr
+
+		** loop ends since the current is nullptr **
+	*/
 	void LinkedList::reverse() {
 		Node *&current = this->node;
 		Node *prev = nullptr;
@@ -43,7 +93,7 @@ namespace NS_LinkedList {
 
 	void test() {
 
-		Node* node = new Node(9, new Node(2, new Node(8, new Node(7, new Node(11, new Node(21, new Node(6)))))));
+		Node* node = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
 
 		unique_ptr<LinkedList> llist = make_unique<LinkedList>(node);
 		cout << " [Original] ";
